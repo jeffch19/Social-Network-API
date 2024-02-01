671394d3680940d969a83633174b317de7dae336
jeffch19 <17jcummings@gmail.com>
2024-02-01 18:00:04 -0500

    added user model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address']
+  },
+  thoughts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Thought'
+  }],
+  friends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+});
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const user = model('user', userSchema);
+
+module.exports = user;
