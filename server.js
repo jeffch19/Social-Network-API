ba3336a760f2923f29d3563c19b99c03fd10045b
jeffch19 <17jcummings@gmail.com>
2024-02-02 15:41:07 -0500

    added to server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express'); 
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/socialnetworkapi';
+
+app.use(express.json());
+app.use('/api', routes);
+
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+ }).then(() => {
+  console.log('MongoDB Connected');
+  app.listen(PORT, () => {
+     console.log(`Server running on port ${PORT}`);
+  });
+ }).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+ });
