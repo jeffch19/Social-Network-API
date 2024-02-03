b97e046cc63abdae6d9fae24ab1860b2df01f6e1
jeffch19 <17jcummings@gmail.com>
2024-02-03 15:32:17 -0500

    removed duplicate in server

--- server.js
+++ server.js
@@ -14,11 +14,6 @@ app.use('/api', routes);
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
  }).then(() => {
   console.log('MongoDB Connected');
   app.listen(PORT, () => {
